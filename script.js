8f51778a9e9367c51d1787e443efc71a44b9b086
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-24 19:55:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-const inputs = document.querySelectorAll('.controls input');
+// const inputs = document.querySelectorAll('.controls input');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+//     function handleUpdate() {
+//       const suffix = this.dataset.sizing || '';
+//       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+//     }
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+//     inputs.forEach(input => input.addEventListener('change', handleUpdate));
+//     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
+// Get the necessary elements
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress__filled');
+const toggleButton = document.querySelector('.toggle');
+const volumeSlider = document.querySelector('input[name="volume"]');
+const playbackRateSlider = document.querySelector('input[name="playbackRate"]');
+const skipButtons = document.querySelectorAll('.player__button[data-skip]');
+
+// Play/Pause functionality
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update the play/pause button text
+function updateToggleButton() {
+  toggleButton.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+// Update the progress bar
+function updateProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progress.style.flexBasis = `${percent}%`;
+}
+
+// Update the video volume
+function updateVolume() {
+  video.volume = volumeSlider.value;
+}
+
+// Update the video playback speed
+function updatePlaybackRate() {
+  video.playbackRate = playbackRateSlider.value;
+}
+
+// Skip the video forward or backward
+function skipVideo() {
+  const skipSeconds = parseFloat(this.dataset.skip);
+  video.currentTime += skipSeconds;
+}
+
+// Add event listeners
+video.addEventListener('click', togglePlay);
+toggleButton.addEventListener('click', togglePlay);
+video.addEventListener('play', updateToggleButton);
+video.addEventListener('pause', updateToggleButton);
+video.addEventListener('timeupdate', updateProgress);
+volumeSlider.addEventListener('input', updateVolume);
+playbackRateSlider.addEventListener('input', updatePlaybackRate);
+skipButtons.forEach(button => button.addEventListener('click', skipVideo));
